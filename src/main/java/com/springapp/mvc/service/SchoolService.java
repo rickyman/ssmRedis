a142fb4164402b2a79546056e2d4faa9b2f1a448
feige <jinyongfei@tju.edu.cn>
2017-01-06 09:20:43 +0800

    need to complete

--- src/main/java/com/springapp/mvc/service/SchoolService.java
+++ src/main/java/com/springapp/mvc/service/SchoolService.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
@@ -31,36 +33,41 @@ public class SchoolService {
      * @return
      * @throws Exception
      */
-    public TbSchool selectByPrimaryKey(Integer id) throws Exception {
-        return tbSchoolDao.selectByPrimaryKey(id);
+    public TbSchoolList selectByPrimaryKey(int id) throws Exception {
+        return tbSchoolListDao.selectByPrimaryKey(id);
     }
 
     //按照用户查询条件查询学校
     public List<TbSchoolList> getSchoolByCondition(SchoolCondition condition) throws Exception {
         List<TbSchoolList> tbSchoolLists = null;
-        //默认的综合排序  距离*0.4+报名人数*0.3+费用*0.3(干预的以后再做)
-        if (condition.getIsComprehensive()== 1) {
-            //to do
-            return tbSchoolLists;
-        }if(condition.getIsDistance()==1){//按照距离最近排序
 
+      if(condition.getIsDistance()==1){//按照距离最近排序
+            tbSchoolLists=selectAllTbSchool();//从数据库中查询所有的学校
+            getDistance(tbSchoolLists,condition);//计算学校到用户的距离
+            distanceFilter(tbSchoolLists,condition);//距离的过滤
+            if(tbSchoolLists!=null&&tbSchoolLists.size()>0)
+                 Collections.sort(tbSchoolLists);//排序
             return tbSchoolLists;
         }if(condition.getIsNumberOfAppli()==1){//按照驾校的报名人数排序
             tbSchoolLists=orderByOrders();//按照驾校的报名人数排序
             getDistanceAndFilter(tbSchoolLists,condition);//计算学校到用户的距离 并过滤距离
             return tbSchoolLists;
-        }if(condition.getIsfee()==1){//按照驾校价格进行排序
+        }if(condition.getIsfee()==1){//按照驾校价格进行排序 当驾校价格为0或者为空时不予以显示
             tbSchoolLists=orderByFee();//按照驾校价格进行排序
             getDistanceAndFilter(tbSchoolLists,condition);//计算学校到用户的距离 并过滤距离
             return tbSchoolLists;
         }
+        //默认的综合排序  距离*0.4+报名人数*0.3+费用*0.3(干预的以后再做)
+        /**
+         * to do 综合排序的处理
+         */
         return tbSchoolLists;
     }
 
     //距离的过滤
-    public List<TbSchoolList> distanceFilter(List<TbSchoolList> tbSchoolLists, SchoolCondition condition) throws Exception {
+    public void distanceFilter(List<TbSchoolList> tbSchoolLists, SchoolCondition condition) throws Exception {
         if (tbSchoolLists == null || condition == null)
-            return null;
+            return ;
         //0(表示全城)时设置condition的distance为Max
         if (condition.getDistance() == 0)
             condition.setDistance(Integer.MAX_VALUE);
@@ -72,7 +79,6 @@ public class SchoolService {
             if (tbSchoolList.getDistance() > condition.getDistance())
                 iterator.remove();
         }
-        return tbSchoolLists;
     }
 
     //按照费用进行排序
@@ -104,6 +110,10 @@ public class SchoolService {
     //根据学校名称进行查询
     public List<TbSchoolList> selectSchoolByName(String name) throws Exception {
         return tbSchoolListDao.selectSchoolByName(name);
+    }
+    //查询所有的学校
+    public List<TbSchoolList> selectAllTbSchool() throws  Exception{
+        return tbSchoolListDao.selectAllTbSchool();
     }
 
     //计算学校到用户的距离 并距离过滤
